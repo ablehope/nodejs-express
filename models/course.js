6e6f7b1ab4c043d4d0f6d7f44f902efd1ee888eb
Sasha Prokofyev <ablehope@yandex.by>
2020-04-06 18:29:20 +0300

    added ability open  dynamic page course

--- models/course.js
+++ models/course.js
@@ -53,6 +53,10 @@ class Course {
       )
     })
   }
+  static async getById(id) {
+    const courses = await Course.getAll()
+    return courses.find((c) => c.id === id)
+  }
 }
 
 module.exports = Course;
