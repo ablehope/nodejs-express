c009a95f7109694e001d5ab2832948d73c0c15da
Sasha Prokofyev <ablehope@yandex.by>
2020-04-14 13:16:55 +0300

    added test user

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const homeRoutes = require('./routes/home');
 const cardRoutes = require('./routes/card');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
+const User = require('./models/user');
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -19,6 +20,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('5e949fe2e05d8a095089ae9e');
+        req.user = user;
+        next();
+    } catch(e) {
+        console.log(e);
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
@@ -37,6 +48,16 @@ async function start() {
             useUnifiedTopology: true,
             useFindAndModify: false
             });
+
+            const candidate = await User.findOne();
+            if (!candidate) {
+                const user = new User({
+                    email: 'sasha@gmail.com',
+                    name: 'sasha',
+                    cart: {items: []}
+                })
+                await user.save()
+            }
     
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
