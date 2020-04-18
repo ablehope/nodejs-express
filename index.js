35870f76ab50d2d29b9879bc2d29b3ffbddc37f8
Sasha Prokofyev <ablehope@yandex.by>
2020-04-18 14:43:03 +0300

    added messages to errors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const csrf = require('csurf');
+const flash = require('connect-flash');
 const mongoose = require('mongoose');
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
@@ -13,7 +14,6 @@ const addRoutes = require('./routes/add');
 const ordersRoutes = require('./routes/orders');
 const coursesRoutes = require('./routes/courses');
 const authRoutes = require('./routes/auth');
-const User = require('./models/user');
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user');
 
@@ -45,6 +45,7 @@ app.use(session({
     store: store
 }))
 app.use(csrf());
+app.use(flash());
 app.use(varMiddleware);
 app.use(userMiddleware);
 
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ router.get('/login', (req, res) => {
   res.render('auth/login', {
     title: 'Авторизация',
     isLogin: true,
+    loginError: req.flash('loginError'),
+    registerError: req.flash('registerError')
   })
 })
 
@@ -32,9 +34,11 @@ router.post('/login', async (req, res) => {
           res.redirect('/');
         })
       } else {
+        req.flash('loginError', 'Неверный пароль')
         res.redirect('/auth/login#login')
       }
     } else {
+      req.flash('loginError', 'Такого пользователя не существует')
       res.redirect('/auth/login#login')
     }
   } catch(e) {
@@ -47,6 +51,7 @@ router.post('/register', async (req, res) => {
     const {email, password, repeat, name} = req.body;
     const candidate = await User.findOne({email});
     if(candidate) {
+      req.flash('registerError', 'Пользователь с таким e-mail уже существует')
       res.redirect('/auth/login#register')
     } else {
       const hashPassword = await bcrypt.hash(password, 10);
