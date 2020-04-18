166b1997f541a3b84f0979a5b9b8348a8e07af2d
Sasha Prokofyev <ablehope@yandex.by>
2020-04-18 10:32:07 +0300

    added register  user

--- routes/auth.js
+++ routes/auth.js
@@ -16,15 +16,47 @@ router.get('/logout', (req, res) => {
 })
 
 router.post('/login', async (req, res) => {
-  const user = await User.findById('5e949fe2e05d8a095089ae9e');
-  req.session.user = user;
-  req.session.isAuthenticated = true;
-  req.session.save((err) => {
-    if(err) {
-      throw err
+  try {
+    const {email, password} = req.body;
+    const candidate = await User.findOne({email});
+    if(candidate) {
+      const areSame = password === candidate.password;
+      if(areSame) {
+        req.session.user = candidate;
+        req.session.isAuthenticated = true;
+        req.session.save((err) => {
+          if(err) {
+            throw err
+          }
+          res.redirect('/');
+        })
+      } else {
+        res.redirect('/auth/login#login')
+      }
+    } else {
+      res.redirect('/auth/login#login')
     }
-    res.redirect('/');
-  })
+  } catch(e) {
+    console.log(e)
+  }
+})
+
+router.post('/register', async (req, res) => {
+  try {
+    const {email, password, repeat, name} = req.body;
+    const candidate = await User.findOne({email});
+    if(candidate) {
+      res.redirect('/auth/login#register')
+    } else {
+      const user = new User({
+        email, name, password, cart: {items: []}
+      })
+      await user.save();
+      res.redirect('/auth/login#login');
+    }
+  } catch(e) {
+    console.log(e);
+  }
 })
 
 module.exports = router;
